4c6c885b366554225002f15fbbd7341aba548f92
tinky-666 <1837581774@qq.com>
2025-04-11 16:32:17 +0800

    last

--- reaction.py
+++ reaction.py
@@ -1,7 +1,14 @@
-from gpiozero import LED, Button 
-from time import sleep
+from gpiozero import LED, Button from time import sleep
 from random import uniform
-led = LED(4)
-led.on()
-sleep(uniform(5, 10))
-led.off()
+led=LED(4)
+right_button= Button15)
+left_button = Button(14)
+led.on0
+sleep(uniform(5,10)
+led.off0
+def pressed(button):
+    print(str(button.pin.number) + ' won the game')
+right_button.when_pressed = pressed
+left_button.when_pressed = pressed
+while True:
+    sleep(0.1)
